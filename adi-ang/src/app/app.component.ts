b8a5be7ba448858a9a64219b4e5dcbd162493185
Jose Alberola Torres <josealberolaat@gmail.com>
2022-01-03 14:29:46 +0100

    Primeros pasos proyecto

--- adi-ang/src/app/app.component.ts
+++ adi-ang/src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'adi-ang';
+  title:string = 'Lista de la compra';
+
+  employees = [
+    {'name': 'Jose', position: 'manager', email:"jose@ua"},
+    {'name': 'Javito', position: 'manager', email:"havi@ua"},
+    {'name': 'Pedris', position: 'manager', email:"pedro@ua"},
+  ]
+
+  model:any = {};
+  model2:any = {};
+
+  addEmployee():void{
+    this.employees.push(this.model);
+  }
+
+  deleteEmployee(i:number):void{
+
+  }
+
+  myValue: number = 0;
+
+  editEmployee(i: number):void{
+    this.model2.name = this.employees[i].name;
+    this.model2.position = this.employees[i].position;
+    this.model2.email = this.employees[i].email;
+    this.myValue = i;
+  }
+
+  updateEmployee():void{
+    console.log(this.model2);
+  }
 }
